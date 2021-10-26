63ae09de1957674f2e0beeb20c43a5a699228e10
chintanshahv3cube <chintan.shah@v3cube.in>
2021-10-26 09:42:06 +0530

    commit all code

---
+++ app/src/main/java/com/example/customlib/CustomTestMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.customlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomTestMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_activity_main);
+    }
+}
