6ada6fedbf68068d0ae0df41a5f55f23a7325602
chintanshahv3cube <chintan.shah@v3cube.in>
2022-07-29 15:59:16 +0530

    main commit

--- app/src/main/java/com/example/customlib/CustomTestMainActivity.java
+++ app/src/main/java/com/example/customlib/CustomTestMainActivity.java
@@ -3,6 +3,7 @@ package com.example.customlib;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class CustomTestMainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class CustomTestMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_activity_main);
+
+        Toast.makeText(this, "main testing", Toast.LENGTH_SHORT).show();
+
+        String data="testing";
+
     }
 }
